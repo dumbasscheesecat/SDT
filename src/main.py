3b308901fe6be0e716e4d295f32622b383f1719c
minhNguyen85288842 <minhng@class-docker>
2025-02-06 20:08:38 +0000

    Initial setup of the class, defined the input variables

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
 import numpy as np
+
+class SignalDetection:
+	def __init__(self, hits, misses, falseAlarms, correctRejections)
+	self.hits = hits
+	self.misses =  misses
+	self.falseAlarms = falseAlarms
+	self.correctRejections = correctRejections
+
+	def hit_rate(self):
+	
+	def false_alarm_rate(self):
+	
+	def d_prime(self):
+
+	def criterion(self):
