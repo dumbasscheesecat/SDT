9c8aaa61676cc4ac289d81785b078dc1e611a756
minhNguyen85288842 <minhng@class-docker>
2025-02-07 09:45:53 +0000

    fix some syntax errors

--- src/sdt.py
+++ src/sdt.py
@@ -1,16 +1,18 @@
 import numpy as np
 
 class SignalDetection:
-	def __init__(self, hits, misses, falseAlarms, correctRejections)
-	self.hits = hits
-	self.misses =  misses
-	self.falseAlarms = falseAlarms
-	self.correctRejections = correctRejections
+	def __init__(self, hits, misses, falseAlarms, correctRejections):
+		self.hits = hits
+		self.misses =  misses
+		self.falseAlarms = falseAlarms
+		self.correctRejections = correctRejections
 
 	def hit_rate(self):
 		return (self.hits) / (self.hits + self.misses)
+
 	def false_alarm_rate(self):
 		return (self.falseAlarms) / (self.falseAlarms + self.correctRejections)
+
 	def d_prime(self):
 
 	def criterion(self):
